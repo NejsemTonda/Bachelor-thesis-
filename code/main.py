f5f27624fa1c9c82b6f36e7eda908db079123622
nejsemTonda <vaclav@krnak.cz>
2024-03-04 17:12:08 +0100

    test
    test2

--- code/main.py
+++ code/main.py
@@ -6,17 +6,19 @@ from levels import LevelFactory
 from Box2D.b2 import vec2
 from tqdm import tqdm
 
+
 level = LevelFactory.level1()
 a = Agent(SimpleGenome(level))
 b = Agent(SimpleGenome(level))
 c = Agent(SimpleGenome(level))
 
-c.genome.clicks = [vec2(10,10), vec2(20,10), vec2(30,10), vec2(25,15), vec2(20,10), vec2(15,15), vec2(10,10),vec2(15,15), vec2(25,15)]
-c.genome.types = [Type.road, Type.road, Type.none, Type.none, Type.none, Type.none, Type.none, Type.none, Type.none]
+c.genome.clicks = [vec2(15,10), vec2(17,10), vec2(15,7), vec2(17,10), vec2(21,10), vec2(25,10), vec2(10,10),vec2(15,15), vec2(25,15)]
+c.genome.types = [Type.road, Type.plank, Type.none, Type.road, Type.road, Type.none, Type.none, Type.none, Type.none]
 
-#eva.fitness.simple_fitness(c, LevelFactory.level1(), draw=True)
+eva.fitness.simple_fitness(c, LevelFactory.level1(), draw=True)
+quit()
 
-agents = [Agent(SimpleGenome(level)) for _ in range(10)]
+agents = [Agent(SimpleGenome(level, lenght=20)) for _ in range(1000)]
 
 fits = []
 for a in tqdm(agents):
