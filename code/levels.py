a74e2b4c0839dafd8ccfe9c2e6ea19f2ae79180e
nejsemTonda <vaclav@krnak.cz>
2024-03-06 11:03:22 +0100

    easier level1

--- code/levels.py
+++ code/levels.py
@@ -18,19 +18,19 @@ class LevelFactory:
 		env = Environment()
 		e = 0.1
 		env.add_ground(
-			[(0,10+e), (10,10+e), (10,0)],
-			anchors=[vec2(10,10), vec2(10, 7)] 
+			[(0,10+e), (15,10+e), (15,0)],
+			anchors=[vec2(15,10), vec2(15, 7)] 
 		)
 		env.add_ground(
-			[(40,10+e), (30,10+e), (30,0)],
-			anchors=[vec2(30,10), vec2(30, 7)] 
+			[(40,10+e), (25,10+e), (25,0)],
+			anchors=[vec2(25,10), vec2(25, 7)] 
 		)
 		
 		car = env.add_car(vec2(5,11+e), density=3)
 
 		goal = vec2(38,12)
 
-		x_bound = (0,40)
-		y_bound = (0,30)
+		x_bounds = (0,40)
+		y_bounds = (0,30)
 
 		return Level(env, car, goal, x_bounds, y_bounds)
