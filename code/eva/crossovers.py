7ae32a21409bbe52778e6c534fb73efa00a9cb85
nejsemTonda <vaclav@krnak.cz>
2024-04-27 16:34:43 +0200

    crossover for graph encoding

--- code/eva/crossovers.py
+++ code/eva/crossovers.py
@@ -1,5 +1,6 @@
 import random
-from eva.agents import Agent, SimpleGenome, KnapsackGenome
+from eva.agents import Agent, SimpleGenome, KnapsackGenome, Type
+import copy
 
 
 def vector_cx(a, b, points=None):
@@ -38,8 +39,8 @@ def n_point(agents, n=1):
     assert n < 2, "not implemeted yet"
     offspring = []
     for i in range(len(agents)//2):
-        parent1 = agents[i].genome
-        parent2 = agents[i*2+1].genome
+        parent1 = copy.deepcopy(agents[i].genome)
+        parent2 = copy.deepcopy(agents[i*2+1].genome)
 
         points = [random.randint(0, len(agents[0].genome.types)) for _ in range(n)]
 
@@ -50,8 +51,66 @@ def n_point(agents, n=1):
         offspring.append(Agent(SimpleGenome(child2_clicks, child2_types)))
     return offspring
 
-def graph_cx(agents, n=1):
+def graph_cx(agents, phi=1):
     return agents
+    def split(agent, x=None, y=None):
+        set1 = []
+        set2 = []
+        assert x is not None and y is not None, "can only cross agent by one line"
+            
+        for node in agent.genome.nodes:
+            if x is not None:
+                if node.pos[0] > x:
+                    set1.append(node)
+                else:
+                    set2.append(node)
+            if y is not None:
+                if node.pos[1] > y:
+                    set1.append(node)
+                else:
+                    set2.append(node)
+        for node in agent.genome.node:
+            for e in node.edges:
+                if node in set1 and e in set2 or node in set2 and e in set1:
+                    node.genome.remove(e)
+                    if (e, node, Type.road) in agents.genome.edges:
+                        agents.genome.edges.remove((e, node, Type.road))
+                    elif (node, e, Type.road) in agents.genome.edges:
+                        agents.genome.edges.remove((node, e, Type.road))
+                    elif (node, e, Type.plank) in agents.genome.edges:
+                        agents.genome.edges.remove((node, e, Type.plank))
+                    elif (e, node, Type.plank) in agents.genome.edges:
+                        agents.genome.edges.remove((e, node, Type.plank))
+
+        return set1, set2
+
+    offspring = []
+    for i in range(len(agents)//2):
+        parent1 = copy.deepcopy(agents[i].genome)
+        parent2 = copy.deepcopy(agents[i*2+1].genome)
+        max_x = max([node.pos[0] for node in parent1.genome.nodes] + [node.pos[0] for node in parent2.genome.nodes])
+        min_x = min([node.pos[0] for node in parent1.genome.nodes] + [node.pos[0] for node in parent2.genome.nodes])
+        max_y = max([node.pos[1] for node in parent1.genome.nodes] + [node.pos[1] for node in parent2.genome.nodes])
+        min_y = max([node.pos[1] for node in parent1.genome.nodes] + [node.pos[1] for node in parent2.genome.nodes])
+
+        use_y = random.random() < 0.5
+        point = random.randint(min_y if use_y else min_x, min_y if use_y else max_x) 
+
+        p1_s1, p1_s2 = split(parent1, y=point) if use_y else split(parent1, x=point)
+        p2_s1, p2_s2 = split(parent2, y=point) if use_y else split(parent2, x=point)
+
+        child1 = GraphGenome(p1_s1+p2_s2, [])
+        child2 = GraphGenome(p2_s1+p1_s2, [])
+
+        offspring.append(child1)
+        offspring.append(child2)
+
+    return offspring
+
+
+
+
+
 
 if __name__ == "__main__":
     a = [1,2,3,4,5]
